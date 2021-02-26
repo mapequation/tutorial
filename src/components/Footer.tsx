5bbb4e07628f2852213bbbed8fd1f9f54dcd81ee
Anton Eriksson <anton.eriksson@umu.se>
2021-02-26 07:55:05 +0100

    fix: Improve small screen layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="mt-96 h-52 py-10 border-t-4 border-gray-100">
-      <ul className="flex flex-row justify-center gap-x-20">
+      <ul className="flex flex-col text-center sm:flex-row justify-center gap-x-20">
         <li>
           <a href="//mapequation.org">mapequation.org</a>
         </li>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,10 +8,13 @@ import Header from './Header';
 import Footer from './Footer';
 import useOnScreen from '../hooks/useOnScreen';
 import Button from './Button';
+import hairball from '../images/hairball.png';
 import globe from '../images/globe.png';
 import map1 from '../images/map-1.png';
 import map2 from '../images/map-2.png';
 import map3 from '../images/map-3.png';
+import compressionTop from '../images/compression-top.png';
+import compressionBottom from '../images/compression-bottom.png';
 import { animated, useChain, useSpring } from 'react-spring';
 
 export default function Layout(props: { network: NetworkModel }) {
@@ -29,6 +32,7 @@ export default function Layout(props: { network: NetworkModel }) {
 
   const startRandomWalk = () => {
     if (walkStarted) return;
+    setRate(Rate.Visits);
     const id = window.setInterval(() => network.walker.step(), interval);
     setIntervalId(id);
   };
@@ -39,6 +43,7 @@ export default function Layout(props: { network: NetworkModel }) {
   };
 
   const resetRandomWalk = () => {
+    setRate(Rate.None);
     stopRandomWalk();
     network.walker.reset();
   };
@@ -62,16 +67,6 @@ export default function Layout(props: { network: NetworkModel }) {
 
     return 1 / network.numNodes;
   };
-
-  const intersected = () => {
-    setRate(Rate.Visits);
-    if (!walkStarted) {
-      //startRandomWalk();
-    }
-  };
-
-  const rateWrapperRef = useRef<HTMLDivElement>(null);
-  const isRateVisible = useOnScreen(rateWrapperRef, 1, intersected);
 
   const mapsWrapperRef = useRef<HTMLDivElement>(null);
   const isMapsVisible = useOnScreen(mapsWrapperRef, 0);
@@ -136,21 +131,14 @@ export default function Layout(props: { network: NetworkModel }) {
   ];
 
   return (
-    <div className="container mx-auto text-gray-800 text-xl">
+    <div className="container mx-auto px-5 text-gray-800 text-xl">
       <Header />
 
-      <div className="grid grid-cols-4 gap-x-5 my-20 md:gap-x-10 lg:gap-x-20">
-        <div className="flex flex-col gap-y-8 items-center my-12 mx-auto">
-          <figure>
-            <img
-              className="rounded-full"
-              src="//picsum.photos/400/400.jpg?grayscale"
-              alt="Hairball graph"
-            />
-          </figure>
-          <figcaption>placeholder hairball</figcaption>
+      <div className="grid sm:grid-cols-4 mb-48 gap-x-5 my-20 md:gap-x-10 lg:gap-x-20">
+        <div className="w-2/3 sm:w-auto mx-auto sm:my-12">
+          <img className="rounded-full" src={hairball} alt="Hairball graph" />
         </div>
-        <div className="col-span-3">
+        <div className="md:col-span-3">
           <h1>
             <span className="font-serif font-thin tracking-tight italic text-indigo-400">
               Problem
@@ -170,75 +158,90 @@ export default function Layout(props: { network: NetworkModel }) {
         </div>
       </div>
 
-      <h2>What do we want to understand?</h2>
+      <div className="mb-48">
+        <h2>What do we want to understand?</h2>
 
-      <p>
-        Like maps simplify and highlight important objects such as
-        neighborhoods, cities, streets, and highways from high-resolution
-        satellite images to help us navigate and explore our surroundings,{' '}
-        <strong>we want to identify functional modules</strong> and how they are
-        connected from large networks.
-      </p>
+        <p>
+          Like maps simplify and highlight important objects such as
+          neighborhoods, cities, streets, and highways from high-resolution
+          satellite images to help us navigate and explore our surroundings,{' '}
+          <strong>we want to identify functional modules</strong> and how they
+          are connected from large networks.
+        </p>
 
-      <div
-        ref={mapsWrapperRef}
-        className="grid grid-cols-4 gap-x-5 my-28 md:gap-x-10 lg:gap-x-20 relative"
-      >
-        <div>
-          <img
-            className="object-cover rounded-full shadow-xl"
-            src={globe}
-            alt="The Earth"
-          />
-        </div>
-        {isMapsVisible &&
-          mapProps.map((props, i) => (
-            // @ts-ignore
-            <animated.div className="relative" style={props}>
-              <img
-                src={maps[i].src}
-                alt={maps[i].alt}
-                className="object-cover shadow-lg rounded-lg"
-              />
-            </animated.div>
-          ))}
-      </div>
-      <div className="grid grid-cols-2 gap-10">
-        <div>
-          <div className="sticky top-20">
-            <Network
-              network={network}
-              getRate={getRate}
-              rate={rate}
-              showLabels={false}
-              showModules={false}
+        <div
+          ref={mapsWrapperRef}
+          className="grid grid-cols-4 gap-x-5 my-28 md:gap-x-10 lg:gap-x-20 relative"
+        >
+          <div>
+            <img
+              className="object-cover rounded-full shadow-xl"
+              src={globe}
+              alt="The Earth"
             />
           </div>
+          {isMapsVisible &&
+            mapProps.map((props, i) => (
+              // @ts-ignore
+              <animated.div className="relative" style={props}>
+                <img
+                  src={maps[i].src}
+                  alt={maps[i].alt}
+                  className="object-cover shadow-lg rounded-lg"
+                />
+              </animated.div>
+            ))}
         </div>
+      </div>
 
-        <div>
+      <div className="flex flex-row flex-wrap lg:flex-nowrap gap-x-20 mb-48 items-center">
+        <div className="lg:w-3/5">
+          <h2>Network flows</h2>
           <p>
             We are often not interested in the raw network itself but the{' '}
             <strong>flows</strong> on the network.
           </p>
-          <p>
-            Flows of ideas in social networks, passenger flows in traffic
-            networks, money flows in transaction networks&hellip;
-          </p>
+          <ul className="ml-6 text-gray-600 list-disc">
+            <li>Flows of ideas in social networks</li>
+            <li>Passenger flows in traffic networks</li>
+            <li>Money flows in transaction networks</li>
+          </ul>
           <p>
             The flows connect nodes beyond nearest neighbors and interconnect
             the entire system. Flows tend to stay within certain groups of nodes
             for a relatively long time before exiting.
           </p>
           <p>These groups are the flow modules we are interested in.</p>
-          <div className="flex mb-96 my-12">
+          <p>
+            We can simulate the flows using a <strong>random walk</strong> on
+            the network.
+          </p>
+
+          <div className="flex flex-row space-x-4 my-10">
+            <Button className="button" onClick={stepRandomWalk}>
+              Step
+            </Button>
             <Button
               className="button button--primary"
-              onClick={startRandomWalk}
+              onClick={toggleRandomWalk}
             >
-              Start random walk
+              {walkStarted ? 'Stop Random Walk' : 'Start Random Walk'}
             </Button>
           </div>
+        </div>
+        <div className="w-4/5 lg:w-2/5 ">
+          <Network
+            network={network}
+            getRate={getRate}
+            rate={rate}
+            showLabels={false}
+            showModules={false}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-row flex-wrap md:flex-nowrap gap-x-20 mb-48">
+        <div>
           <h1>
             <span className="font-serif font-thin tracking-tight italic text-indigo-400">
               Solution
@@ -254,28 +257,66 @@ export default function Layout(props: { network: NetworkModel }) {
             </strong>{' '}
             for finding modules in large networks.
           </p>
-          <div ref={rateWrapperRef} className="mb-96">
-            <Rates
-              network={network}
-              getRate={getRate}
-              rate={rate}
-              showModules={false}
-            />
-            <div className="flex flex-row space-x-4 my-10">
-              <Button className="button" type="reset" onClick={resetRandomWalk}>
-                Reset
-              </Button>
-              <Button className="button" onClick={stepRandomWalk}>
-                Step
-              </Button>
-              <Button
-                className="button button--primary"
-                onClick={toggleRandomWalk}
-              >
-                {walkStarted ? 'Stop' : 'Start'}
-              </Button>
-            </div>
+          <h2>Compression</h2>
+          <p>
+            This goal takes us to fundamental principles of cartography and
+            information theory. Information-theoretic compression algorithms
+            identify regularities to compress data.{' '}
+            <strong>
+              The more regularities they find, the better they can compress the
+              data
+            </strong>
+            .
+          </p>
+        </div>
+        <div className="mt-24 flex flex-col items-center space-y-6">
+          <div>
+            5.8 MB (tiff) &rarr; <strong>0.91 MB</strong> (tiff + LZW)
           </div>
+          <div>
+            <img
+              className="rounded-lg shadow-lg"
+              src={compressionTop}
+              alt="top"
+            />
+          </div>
+          <div>
+            <img
+              className="rounded-lg shadow-lg"
+              src={compressionBottom}
+              alt=""
+            />
+          </div>
+          <div>
+            5.8 MB (tiff) &rarr; <strong>2.8 MB</strong> (tiff + LZW)
+          </div>
+        </div>
+      </div>
+
+      <h2 className="font-light mx-auto text-center lg:w-3/5 mb-48">
+        We are after the <span className="text-indigo-500">regularities</span>{' '}
+        and use the <span className="text-indigo-500">compression rate</span> to
+        measure how good we are at finding them.
+      </h2>
+
+      <h2>Huffman coding</h2>
+      <p>
+        To use the machinery of information theory, we describe the random
+        walker with a binary message.
+      </p>
+      <p>
+        Huffman coding (Like Morse code, more frequently used symbols should be
+        shorter).
+      </p>
+
+      <Rates
+        network={network}
+        getRate={getRate}
+        rate={rate}
+        showModules={false}
+      />
+      <div className="mb-96">
+        {/*
           <h2>Measuring the description length</h2>
           <CodeBooks network={network} />
           <br />
@@ -289,7 +330,7 @@ export default function Layout(props: { network: NetworkModel }) {
           {network.mapequation.moduleCodelength.toFixed(3)} {'bits'}
           <br />
           {'Codelength'} {network.mapequation.codelength.toFixed(3)} {'bits'}
-        </div>
+          */}
       </div>
       <Footer />
     </div>
