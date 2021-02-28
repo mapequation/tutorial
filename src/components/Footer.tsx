04de2e2effcabf0b423ceb3f2c602124c6acb6e3
Anton Eriksson <anton.eriksson@umu.se>
2021-02-28 18:45:13 +0100

    style: More padding in footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { HTMLProps } from 'react';
+
+const ListLink = ({ children, ...props }: HTMLProps<HTMLAnchorElement>) => (
+  <li>
+    <a className="inline-block px-10 py-2 hover:text-gray-600" {...props}>
+      {children}
+    </a>
+  </li>
+);
 
 export default function Footer() {
   return (
     <footer className="mt-96 h-52 py-10">
       <ul className="flex flex-col md:flex-row justify-center text-center divide-y md:divide-x md:divide-y-0 divide-gray-300">
-        <li className="px-10 py-2">
-          <a href="//mapequation.org">mapequation.org</a>
-        </li>
-        <li className="px-10 py-2">
-          <a href="//www.mapequation.org/about.html">contact</a>
-        </li>
-        <li className="px-10 py-2">
-          <a href="//github.com/mapequation">github</a>
-        </li>
+        <ListLink href="//mapequation.org">mapequation.org</ListLink>
+        <ListLink href="//mapequation.org/about.html">contact</ListLink>
+        <ListLink href="//github.com/mapequation">github</ListLink>
       </ul>
     </footer>
   );
