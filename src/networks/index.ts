56c33219a700f0c0887eea4f512427608064faee
Anton Eriksson <anton.eriksson@umu.se>
2022-12-15 18:55:20 +0100

    feat: Add undirected network

--- src/networks/index.ts
+++ src/networks/index.ts
@@ -1,2 +1,3 @@
 export { default as modular_wd } from "./modular_wd";
 export { default as modular_wd_json } from "./modular_wd_json.json";
+export { default as modular_w_json } from "./modular_w_json.json";
