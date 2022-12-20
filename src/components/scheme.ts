3f6f84796bd88f6e1bfebf7fff6d6f5ef2988b2a
Anton Eriksson <anton.eriksson@umu.se>
2022-12-20 15:22:03 +0100

    feat: Add schemeFromInterpolator

--- src/components/scheme.ts
+++ src/components/scheme.ts
@@ -3,4 +3,12 @@ import { schemePastel2, schemeSet2 } from "d3";
 const scheme = schemePastel2 as string[];
 const schemeAlt = schemeSet2 as string[];
 
+export function schemeFromInterpolator(n: number, interpolator: (t: number) => string): string[] {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(interpolator(i / (n - 1)));
+  }
+  return result;
+}
+
 export { scheme, schemeAlt };
